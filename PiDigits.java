643ec67519d8cfa41df9fd8f702bdc541e1ba467
Dore Chen <dore_chen@hotmail.com>
2016-08-02 19:03:49 +1200

    Can print out correct number of requested digits after the decimal point.

--- PiDigits.java
+++ PiDigits.java
@@ -6,16 +6,16 @@ public class PiDigits {
 
 		int digits = Integer.parseInt(args[0]);
 		String piString = String.valueOf(Math.PI);
+		int length = piString.length();
+		length = length - 2;
 
 		try {
 			System.out.println(Integer.parseInt(args[0]) + " digit(s) of Pi:");
-			//int digits = Integer.parseInt(args[0]);
-			//String piString = String.valueOf(Math.PI);
-			System.out.println(piString.substring(0, digits));
+			System.out.println(piString.substring(0, digits+2));
 		} catch (NumberFormatException n){
 			System.err.println("Invalid input. Please enter an integer.");
 		} catch (StringIndexOutOfBoundsException s) {
-			System.err.println("Invalid input. Please enter a number less than " + piString.length() + ".");
+			System.err.println("Invalid input. Please enter a number less than or equal to " + length + ".");
 		}
 
 		//System.out.println(String.valueOf(Math.PI));
