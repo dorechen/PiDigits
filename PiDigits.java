86978bd4b409d8d7519308a38d82bd81e309da9b
Dore Chen <dore_chen@hotmail.com>
2016-08-02 18:23:46 +1200

    Can print the requested digits.

--- PiDigits.java
+++ PiDigits.java
@@ -4,11 +4,20 @@ public class PiDigits {
 
 	public static void main (String[] args) {
 
+		int digits = Integer.parseInt(args[0]);
+		String piString = String.valueOf(Math.PI);
+
 		try {
-			System.out.println(Integer.parseInt(args[0]));
+			System.out.println(Integer.parseInt(args[0]) + " digit(s) of Pi:");
+			//int digits = Integer.parseInt(args[0]);
+			//String piString = String.valueOf(Math.PI);
+			System.out.println(piString.substring(0, digits));
 		} catch (NumberFormatException n){
 			System.err.println("Invalid input. Please enter an integer.");
+		} catch (StringIndexOutOfBoundsException s) {
+			System.err.println("Invalid input. Please enter a number less than " + piString.length() + ".");
 		}
+
 		//System.out.println(String.valueOf(Math.PI));
 		//System.out.println(Double.toString(Math.PI));
 		//System.out.println(Double.valueOf(Math.PI).toString());
